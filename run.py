000c0b540b3fe455af98957d4e1c3f966307111f
SoLee1221 <siqbal1221@icloud.com>
2021-08-10 12:56:37 +0000

    insert random and random row and col function

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+from random import randint
+
 board = []
 
 for i in range(0, 5):
@@ -8,3 +10,9 @@ def whole_board(board):
         print(" ".join(i))
 
 whole_board(board)
+
+def random_row(board):
+    return randint(0, len(board) -1)
+
+def random_col(board):
+    return randint(0, len(board[0]) -1)
