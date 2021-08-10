e530c50c7e2c9b1c8192d8e539574a8f4d4b093a
SoLee1221 <siqbal1221@icloud.com>
2021-08-10 13:21:57 +0000

    add if else statment if guessing ship row and col returns a string

--- run.py
+++ run.py
@@ -16,3 +16,18 @@ def random_row(board):
 
 def random_col(board):
     return randint(0, len(board[0]) -1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+guess_row = int(input("guess_row: "))
+guess_col = int(input("guess_col: "))
+
+print(ship_row)
+print(ship_col)
+
+if guess_row == ship_row and guess_col == ship_col:
+    print("Congratulations you have sunk my ship!!!")
+else:
+    print("You missed my ship!")
+    board[guess_row][guess_col] = "x"
+    whole_board(board)
