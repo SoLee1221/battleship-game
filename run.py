8d4707e5b512f84d0d5891a17f7f3fd960edc1a4
SoLee1221 <siqbal1221@icloud.com>
2021-08-11 11:50:20 +0000

    create a for loop that repeats the game with limited amount of rounds

--- run.py
+++ run.py
@@ -21,24 +21,27 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-guess_row = int(input("guess_row: "))
-guess_col = int(input("guess_col: "))
-
 print(ship_row)
 print(ship_col)
 
-if guess_row == ship_row and guess_col == ship_col:
-    print("Congratulations you have sunk my ship!!!")
-else:
-    """
-    Add if statment for users 
-    Input numbers that are bigger than the board
-    """
-    if guess_row > board_size or guess_col > board_size:
-        print("Oops thats not in the ocean range please try again!")
-    elif board[guess_row][guess_col] == "x":
-        print("You already guessed this one already!")
+for turn in range(4):
+    print("Round", turn + 1)
+
+    guess_row = int(input("guess_row: "))
+    guess_col = int(input("guess_col: "))
+
+    if guess_row == ship_row and guess_col == ship_col:
+        print("Congratulations you have sunk my ship!!!")
     else:
-        print("You missed my ship!")
-        board[guess_row][guess_col] = "x"
-        whole_board(board)
+        """
+        Add if statment for users 
+        Input numbers that are bigger than the board
+        """
+        if guess_row > board_size or guess_col > board_size:
+            print("Oops thats not in the ocean range please try again!")
+        elif board[guess_row][guess_col] == "x":
+            print("You already guessed this one already!")
+        else:
+            print("You missed my ship!")
+            board[guess_row][guess_col] = "x"
+            whole_board(board)
