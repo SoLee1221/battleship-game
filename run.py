d1149fcab8ae4c40886ff4879a22c701ac36fba4
SoLee1221 <siqbal1221@icloud.com>
2021-08-11 14:13:59 +0000

    add while loop and try statment for input not being integers

--- run.py
+++ run.py
@@ -27,23 +27,41 @@ print(ship_col)
 for turn in range(4):
     print("Round", turn + 1)
 
-    guess_row = int(input("guess_row: "))
-    guess_col = int(input("guess_col: "))
+    guess_row = input("guess_row: ")
+    guess_col = input("guess_col: ")
+    valid_input = True
+
+    try:
+        guess_row = int(guess_row)
+    except ValueError:
+        print("That's not a valid input!")
+        valid_input = False
+
+    while not valid_input:
+        guess_row = input("guess_col: ")
+        valid_input = True
+        try:
+            guess_row = int(guess_row)
+        except ValueError:
+            print("That's not a valid input!")
+            valid_input = False
+
 
     if guess_row == ship_row and guess_col == ship_col:
         print("Congratulations you have sunk my ship!!!")
+        break
     else:
         """
-        Add if statment for users 
+        Add if statment for user 
         Input numbers that are bigger than the board
         """
         if guess_row > board_size or guess_col > board_size or guess_row < 0 or guess_col < 0:
             print("Oops thats not in the ocean range please try again!")
-        elif board[guess_row][guess_col] == "x":
+        elif board[guess_row][guess_col] == "X":
             print("You already guessed this one already!")
         else:
             print("You missed my ship!")
-            board[guess_row][guess_col] = "x"
+            board[guess_row][guess_col] = "X"
             if turn == 3:
                 print("Game Over!!")
             whole_board(board)
