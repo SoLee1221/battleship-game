4c485e571716210dfb761ebde4f8d3a5a96b36b0
SoLee1221 <siqbal1221@icloud.com>
2021-08-15 22:07:54 +0000

    fix game over function

--- run.py
+++ run.py
@@ -23,26 +23,28 @@ ship_row = random_row(board)
 ship_col = random_col(board)
 print(ship_row)
 print(ship_col)
+number_of_rounds = 10
 
 """
 Create try function for incorrect input and correct input by user
-col and row example none integer or str
+col and row example none integer or str 
+Also
+Repeats invalid input until user inputs 
+correct data to progress further in the game
+
 """
 def guess_input():
     guess_row = input("guess_row: ")
     valid_input = True
     try:
         guess_row = int(guess_row)
-    if guess_row >= board_size or guess_row < 0:
-        print("Invalid input")
-        valid_input = False
+        if guess_row > board_size or guess_row < 0:                    
+            print("Invalid input")
+            valid_input = False
     except ValueError:
-        print("That's not a valid input!")
+        print("That's not a number")
         valid_input = False
-"""
-Repeats invalid input until user inputs 
-correct data to progress further in the game
-"""
+
     while not valid_input:
         guess_row = input("guess_row: ")
         valid_input = True
@@ -52,27 +54,29 @@ correct data to progress further in the game
                 print("Invalid input")
                 valid_input = False
         except ValueError:
-            print("That's not a valid input!")
+            print("That's not a number")
             valid_input = False
-    return guess_row
-    
+    return guess_input   
 
-def output_game(last_turn):
-    game_over = False
+def output_game():
     if guess_row == ship_row and guess_col == ship_col:
         print("Congratulations you have sunk my ship!!!")
-        break
+        #todo
     else:
         if board[guess_row][guess_col] == "x":
             print("You already guessed this one already!")
         else:
             print("You missed my ship!")
             board[guess_row][guess_col] = "x"
-            if last_turn:
-                print("Game Over!!")
             whole_board(board)
 
-for turn in range(10):
+def last_round():
+    if number_of_rounds == last_round
+    print("Game Over!!!")
+
+for turn in range(number_of_rounds):
     print("Round", turn + 1)
-    guess_input()
-    output_game(last_turn)
+    guess_row = guess_input()
+    guess_col = guess_input()
+    output_game()
+    last_round()
